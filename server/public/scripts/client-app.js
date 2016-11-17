8e7c03bde15591127cff32574cbb2f87d7f453c4
Isaac Halvorson <hello@hisaac.net>
2016-11-16 20:57:36 -0600

    build ajax routes and html selector element

---
+++ server/public/scripts/client-app.js
@@ -0,0 +1,133 @@
+$(document).ready(function () {
+    getBooks();
+
+    // add a book
+    $('#book-submit').on('click', postBook);
+    // delete a book
+    $("#book-list").on('click', '.delete', deleteBook);
+    // update a book
+    $("#book-list").on('click', '.update', updateBook);
+    $('#genreSelector').change(selectGenre);
+});
+/**
+ * Retrieve books from server and append to DOM
+ */
+function getBooks() {
+  $.ajax({
+    type: 'GET',
+    url: '/books',
+    success: function(books) {
+      appendBooks(books);
+    },
+    error: function() {
+      console.log('Database error');
+    }
+  });
+}
+/**
+ * Add a new book to the database and refresh the DOM
+ */
+function postBook() {
+  event.preventDefault();
+
+  var book = {};
+
+  $.each($('#book-form').serializeArray(), function (i, field) {
+    book[field.name] = field.value;
+  });
+  // convert edition to integer
+  book.edition = parseInt(book.edition);
+
+  $.ajax({
+    type: 'POST',
+    url: '/books',
+    data: book,
+    success: function(response) {
+      getBooks();
+    },
+    error: function() {
+      console.log('could not post a new book');
+    }
+  })
+
+}
+
+function deleteBook() {
+  var id = $(this).parent().data('id');
+
+  $.ajax({
+    type: 'DELETE',
+    url: '/books/' + id,
+    success: function(result) {
+      getBooks();
+    },
+    error: function(result) {
+      console.log('could not delete book.');
+    }
+  });
+}
+
+function updateBook() {
+  var id = $(this).parent().data('id');
+  console.log(id);
+
+  // make book object
+  var book = {};
+  var fields = $(this).parent().children().serializeArray();
+  fields.forEach(function(field) {
+    book[field.name] = field.value;
+  });
+
+  $.ajax({
+    type: 'PUT',
+    url: '/books/' + id,
+    data: book,
+    success: function(result) {
+      getBooks();
+    },
+    error: function(result) {
+      console.log('could not update book!');
+    }
+  });
+}
+
+function selectGenre(){
+  var selectedGenre = $('#genreSelector').val();
+
+  $.ajax({
+    type: 'GET',
+    url: '/books/genre/' + selectedGenre,
+    success: function(books) {
+      console.log('returned from server:', books);
+      // appendBooks(books);
+    },
+    error: function() {
+      console.log('Database error');
+    }
+  });
+}
+
+function appendBooks(books) {
+  $("#book-list").empty();
+
+  for (var i = 0; i < books.length; i++) {
+    $("#book-list").append('<div class="row book"></div>');
+    $el = $('#book-list').children().last();
+    var book = books[i];
+    $el.data('id', book.id);
+
+    var convertedDate = book.published.substr(0, 10);
+
+    $el.append('<input type="text" name="title" value="' + book.title + '" />');
+    $el.append('<input type="text" name="author" value="' + book.author + '" />');
+    $el.append('<input type="text" name="genre" value="' + book.genre + '" />');
+    var newDate = $('<input type="date" name="published" />');
+    newDate.val(convertedDate)
+    $el.append(newDate);
+    $el.append('<input type="number" name="edition" value="' + book.edition + '" />');
+    $el.append('<input type="text" name="publisher" value="' + book.publisher + '" />');
+
+    $el.append('<button class="update">Update</button>');
+    $el.append('<button class="delete">Delete</button>');
+  }
+}
